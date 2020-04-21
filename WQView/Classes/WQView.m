1a9703e83a075d792e03c13fde7d28039bbcbaf7
吴琼 <wuqiongaia@163.com>
2020-04-21 19:43:27 +0800

    dddddd

---
+++ WQView/Classes/WQView.m
@@ -0,0 +1,21 @@
+//
+//  WQView.m
+//  WQView_Example
+//
+//  Created by wuqiong on 2020/4/21.
+//  Copyright © 2020 wuqiongaia@163.com. All rights reserved.
+//
+
+#import "WQView.h"
+
+@implementation WQView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
